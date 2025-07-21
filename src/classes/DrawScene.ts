801c2a1dea6a7fb68eeee83050626da6ed92b155
naotaro0123 <n.ikewada@gmail.com>
2025-07-21 16:01:13 +0900

    fix: add Earth

--- src/classes/DrawScene.ts
+++ src/classes/DrawScene.ts
@@ -8,27 +8,216 @@ import {
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
+const SUN_MESH_NAME = 'SUN';
+
 const loadTexture = new THREE.TextureLoader();
 const settings = {
   accelerationOrbit: 1, // 公転スピード
   acceleration: 1, // 自転スピード
   sunIntensity: 1.9, // 太陽の明るさ
 };
+// Earth
+type EarthMoon = {
+  size: number;
+  texture: string;
+  bump: string;
+  orbitSpeed: number;
+  orbitRadius: number;
+  mesh?: THREE.Mesh;
+};
+const earthMoon: EarthMoon[] = [
+  {
+    size: 1.6,
+    texture: '/images/moonmap.jpg',
+    bump: '/images/moonbump.jpg',
+    orbitSpeed: 0.001 * settings.accelerationOrbit,
+    orbitRadius: 10,
+  },
+];
 
 const sunMat = new THREE.MeshStandardMaterial({
   emissive: 0xfff88f,
   emissiveMap: loadTexture.load('./images/sun.jpg'),
   emissiveIntensity: settings.sunIntensity,
 });
-const createSun = (scene: THREE.Scene): void => {
+const createSunMesh = (scene: THREE.Scene): void => {
   const sunSize = 697 / 40; // 40 times smaller scale than earth
   const sunGeom = new THREE.SphereGeometry(sunSize, 32, 20);
   const sun = new THREE.Mesh(sunGeom, sunMat);
+  sun.name = SUN_MESH_NAME;
   scene.add(sun);
 
   //point light in the sun
   const pointLight = new THREE.PointLight(0xfdffd3, 1200, 400, 1.4);
   scene.add(pointLight);
+};
+
+type Ring = {
+  innerRadius: number;
+  outerRadius: number;
+  texture: string;
+};
+const createPlanet = (
+  platenetName: string,
+  size: number,
+  position: number,
+  tilt: number,
+  texture: THREE.Material | string,
+  bump: string | null,
+  ring: Ring | null,
+  atmosphere: string | null,
+  moons: EarthMoon[],
+): THREE.Group => {
+  let material: THREE.Material | THREE.Texture;
+  if (texture instanceof THREE.Material) {
+    material = texture;
+  } else if (bump) {
+    material = new THREE.MeshPhongMaterial({
+      map: loadTexture.load(texture),
+      bumpMap: loadTexture.load(bump),
+      bumpScale: 0.7,
+    });
+  } else {
+    material = new THREE.MeshPhongMaterial({
+      map: loadTexture.load(texture),
+    });
+  }
+
+  const geometry = new THREE.SphereGeometry(size, 32, 20);
+  const planet = new THREE.Mesh(geometry, material);
+  planet.position.x = position;
+  planet.position.z = (tilt * Math.PI) / 180;
+  const planetSystem = new THREE.Group();
+  planetSystem.add(planet);
+
+  const orbitPath = new THREE.EllipseCurve(
+    0,
+    0, // ax, aY
+    position,
+    position, // xRadius, yRadius
+    0,
+    2 * Math.PI, // aStartAngle, aEndAngle
+    false, // aClockwise
+    0, // aRotation
+  );
+  const pathPoints = orbitPath.getPoints(100);
+  const orbitGeometry = new THREE.BufferGeometry().setFromPoints(pathPoints);
+  const orbitMaterial = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+    transparent: true,
+    opacity: 0.03,
+  });
+  const orbit = new THREE.LineLoop(orbitGeometry, orbitMaterial);
+  orbit.rotation.x = Math.PI / 2;
+  planetSystem.add(orbit);
+
+  if (ring) {
+    const ringGeometry = new THREE.RingGeometry(ring.innerRadius, ring.outerRadius, 30);
+    const ringMaterial = new THREE.MeshStandardMaterial({
+      map: loadTexture.load(ring.texture),
+      side: THREE.DoubleSide,
+    });
+    const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
+    planetSystem.add(ringMesh);
+    ringMesh.position.x = position;
+    ringMesh.position.x = 0.5 * Math.PI;
+    ringMesh.rotation.y = (-tilt * Math.PI) / 180;
+  }
+
+  if (atmosphere) {
+    const atmosphereGeometry = new THREE.SphereGeometry(size + 0.1, 32, 20);
+    const atmosphereMaterial = new THREE.MeshPhongMaterial({
+      map: loadTexture.load(atmosphere),
+      transparent: true,
+      opacity: 0.4,
+      depthTest: true,
+      depthWrite: false,
+    });
+    const atmosphereMesh = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+    atmosphereMesh.rotation.z = 0.41;
+    planet.add(atmosphereMesh);
+  }
+
+  for (const moon of moons) {
+    let moonMaterial: THREE.MeshStandardMaterial;
+
+    if (moon.bump) {
+      moonMaterial = new THREE.MeshStandardMaterial({
+        map: loadTexture.load(moon.texture),
+        bumpMap: loadTexture.load(moon.bump),
+        bumpScale: 0.5,
+      });
+    } else {
+      moonMaterial = new THREE.MeshStandardMaterial({
+        map: loadTexture.load(moon.texture),
+      });
+    }
+    const moonGeometry = new THREE.SphereGeometry(moon.size, 32, 20);
+    const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+    const moonOrbitDistance = size * 1.5;
+    moonMesh.position.set(moonOrbitDistance, 0, 0);
+    planetSystem.add(moonMesh);
+    moon.mesh = moonMesh;
+  }
+
+  const planet3d = new THREE.Group();
+  planet3d.add(planetSystem);
+  planet3d.name = platenetName;
+  return planet3d;
+};
+
+const createEarthMesh = (scene: THREE.Scene): void => {
+  const sun = scene.getObjectByName(SUN_MESH_NAME) as THREE.Mesh;
+  // Earth day/night effect shader material
+  const earthMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      dayTexture: { value: loadTexture.load('/images/earth_daymap.jpg') },
+      nightTexture: { value: loadTexture.load('/images/earth_nightmap.jpg') },
+      sunPosition: { value: sun.position },
+    },
+    vertexShader: `
+      varying vec3 vNormal;
+      varying vec2 vUv;
+      varying vec3 vSunDirection;
+
+      uniform vec3 sunPosition;
+
+      void main() {
+        vUv = uv;
+        vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+        vNormal = normalize(modelMatrix * vec4(normal, 0.0)).xyz;
+        vSunDirection = normalize(sunPosition - worldPosition.xyz);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      uniform sampler2D dayTexture;
+      uniform sampler2D nightTexture;
+
+      varying vec3 vNormal;
+      varying vec2 vUv;
+      varying vec3 vSunDirection;
+
+      void main() {
+        float intensity = max(dot(vNormal, vSunDirection), 0.0);
+        vec4 dayColor = texture2D(dayTexture, vUv);
+        vec4 nightColor = texture2D(nightTexture, vUv)* 0.2;
+        gl_FragColor = mix(nightColor, dayColor, intensity);
+      }
+    `,
+  });
+  const earth = createPlanet(
+    'Earth',
+    6.4,
+    90,
+    23,
+    earthMaterial,
+    null,
+    null,
+    '/images/earth_atmosphere.jpg',
+    earthMoon,
+  );
+  scene.add(earth);
 };
 
 export class DrawScene {
@@ -40,7 +229,8 @@ export class DrawScene {
 
   constructor() {
     this.initEnvironment();
-    createSun(this.scene);
+    createSunMesh(this.scene);
+    createEarthMesh(this.scene);
 
     window.addEventListener('resize', this.resizeCanvas);
     this.render();
