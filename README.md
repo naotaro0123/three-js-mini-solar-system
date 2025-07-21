50b2e8462a41de0f74b3ef9cccecd5b8536050be
naotaro0123 <n.ikewada@gmail.com>
2025-07-21 14:56:56 +0900

    fix: remove sun gltf and add sun mesh

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # three-js-mini-solar-system
 
-# use gltf model
-
-- [Sun](https://sketchfab.com/3d-models/sun-9ef1c68fbb944147bcfcc891d3912645)
+Develop Status: InProgress
--- src/classes/DrawScene.ts
+++ src/classes/DrawScene.ts
@@ -1,38 +1,49 @@
 import * as THREE from 'three';
+import {
+  EffectComposer,
+  OutlinePass,
+  RenderPass,
+  UnrealBloomPass,
+} from 'three/examples/jsm/Addons.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
-const SUN_GLTF_PATH = '/gltf/sun/scene.gltf';
+const loadTexture = new THREE.TextureLoader();
+const settings = {
+  accelerationOrbit: 1, // 公転スピード
+  acceleration: 1, // 自転スピード
+  sunIntensity: 1.9, // 太陽の明るさ
+};
+
+const sunMat = new THREE.MeshStandardMaterial({
+  emissive: 0xfff88f,
+  emissiveMap: loadTexture.load('./images/sun.jpg'),
+  emissiveIntensity: settings.sunIntensity,
+});
+const createSun = (scene: THREE.Scene): void => {
+  const sunSize = 697 / 40; // 40 times smaller scale than earth
+  const sunGeom = new THREE.SphereGeometry(sunSize, 32, 20);
+  const sun = new THREE.Mesh(sunGeom, sunMat);
+  scene.add(sun);
+
+  //point light in the sun
+  const pointLight = new THREE.PointLight(0xfdffd3, 1200, 400, 1.4);
+  scene.add(pointLight);
+};
 
 export class DrawScene {
   renderer = new THREE.WebGLRenderer();
   scene = new THREE.Scene();
-  camera: THREE.PerspectiveCamera;
-  controls: OrbitControls;
+  camera!: THREE.PerspectiveCamera;
+  controls!: OrbitControls;
+  composer!: EffectComposer;
 
   constructor() {
-    this.renderer.setSize(this.width, this.height);
-    // this.renderer.setClearColor(0x00000, 1.0);
-    this.renderer.setClearColor(0xf3f3f3, 1.0);
-
-    document.body.appendChild(this.renderer.domElement);
+    this.initEnvironment();
+    createSun(this.scene);
 
     window.addEventListener('resize', this.resizeCanvas);
-
-    this.camera = new THREE.PerspectiveCamera(50, this.width / this.height, 1, 1000);
-    this.camera.position.set(0, 5, 50);
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
-    const light = new THREE.AmbientLight(0xffffff, 1);
-    this.scene.add(light);
-
-    const grid = new THREE.GridHelper(10, 5);
-    this.scene.add(grid);
-
-    this.loadGltf(SUN_GLTF_PATH).then(() => {
-      this.render();
-    });
+    this.render();
   }
 
   get width(): number {
@@ -43,18 +54,63 @@ export class DrawScene {
     return window.innerHeight;
   }
 
+  initEnvironment(): void {
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0x00000, 1.0);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(50, this.width / this.height, 1, 1000);
+    this.camera.position.set(0, 5, 50);
+
+    {
+      this.composer = new EffectComposer(this.renderer);
+      this.composer.addPass(new RenderPass(this.scene, this.camera));
+      // outline Pass
+      const outLinePass = new OutlinePass(
+        new THREE.Vector2(this.width, this.height),
+        this.scene,
+        this.camera,
+      );
+      outLinePass.edgeStrength = 3;
+      outLinePass.edgeGlow = 1;
+      outLinePass.visibleEdgeColor.set(0xffffff);
+      outLinePass.hiddenEdgeColor.set(0x190a05);
+      this.composer.addPass(outLinePass);
+      // Bloom Pass
+      const bloomPass = new UnrealBloomPass(
+        new THREE.Vector2(this.width, this.height),
+        1,
+        0.4,
+        0.85,
+      );
+      bloomPass.threshold = 1;
+      bloomPass.radius = 0.9;
+      this.composer.addPass(bloomPass);
+    }
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    {
+      const gui = new GUI();
+      gui.domElement.style.top = '6px';
+      gui.domElement.style.right = '6px';
+      gui.add(settings, 'accelerationOrbit', 0, 10);
+      gui.add(settings, 'acceleration', 0, 10);
+      gui.add(settings, 'sunIntensity', 1, 10).onChange((value) => {
+        sunMat.emissiveIntensity = value;
+      });
+    }
+
+    const lightAmbient = new THREE.AmbientLight(0x222222, 6);
+    this.scene.add(lightAmbient);
+  }
+
   resizeCanvas = () => {
     this.renderer.setSize(this.width, this.height);
     this.camera.aspect = this.width / this.height;
     this.camera.updateProjectionMatrix();
     this.renderer.setPixelRatio(window.devicePixelRatio);
   };
-
-  async loadGltf(gltfPath: string): Promise<void> {
-    const loader = new GLTFLoader();
-    const gltf = await loader.loadAsync(gltfPath);
-    this.scene.add(gltf.scene);
-  }
 
   render(): void {
     this.renderer.render(this.scene, this.camera);
