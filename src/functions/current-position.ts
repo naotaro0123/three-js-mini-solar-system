3ec2601fb90136ac341fa50c32fd443498636eec
naotaro0123 <n.ikewada@gmail.com>
2025-09-14 14:05:01 +0900

    fix: call nasa api data

--- src/functions/current-position.ts
+++ src/functions/current-position.ts
@@ -8,7 +8,9 @@ type ResponseData = {
   };
 };
 
-export const getCurrentPosition = async () => {
+type ResponseResults = { x: number; y: number; z: number }[];
+
+export const getCurrentPosition = async (): Promise<ResponseResults> => {
   const today = new Date('2025-01-01');
   const tomorrow = addDays(today, 365);
   const startDate = format(today, 'yyyy-MM-dd');
@@ -16,6 +18,7 @@ export const getCurrentPosition = async () => {
   const StepSize = '1d'; // '1d': 1日ごと, '1 mo: 1ヶ月ごと
   // APIエンドポイントのURL(bun-mini-solar-systemリポジトリのサーバーを想定)
   const url = `http://localhost:3000/api/v1/earth-current-position?START_TIME=${startDate}&STOP_TIME=${stopDate}&STEP_SIZE=${StepSize}`;
+  const result: ResponseResults = [];
 
   try {
     const response = await fetch(url);
@@ -35,16 +38,19 @@ export const getCurrentPosition = async () => {
       // 例: 'x'、'y'、'z' 座標を抽出
       const vectors = ephemerisData.split('\n').filter((line) => line.includes('X ='));
       vectors.forEach((line) => {
-        const parts = line.split(/[=,]/).map((p) => p.trim());
-        const x = parseFloat(parts[1]);
-        const y = parseFloat(parts[3]);
-        const z = parseFloat(parts[5]);
-        console.log(`X: ${x}, Y: ${y}, Z: ${z} (AU)`);
+        const parts = line.split(/[X,Y,Z, =]/).filter((p) => p.trim());
+        const x = parseFloat(parts[0]);
+        const y = parseFloat(parts[1]);
+        const z = parseFloat(parts[2]);
+        result.push({ x, y, z });
       });
     } else {
       console.error('データの取得に失敗しました:', data);
     }
   } catch (error) {
     console.error('API呼び出し中にエラーが発生しました:', error);
+  } finally {
+    console.log('result:', result);
+    return result;
   }
 };
