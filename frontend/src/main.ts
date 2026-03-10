edebb1f19b4e96b6087c6e047b2af432fab05cbc
naotaro0123 <n.ikewada@gmail.com>
2026-03-10 20:29:13 +0900

    fix: add saturn(wip)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
-// import { DebugOrbitLine as Simple } from './classes/DebugOrbitLine';
-import { DrawScene as Simple } from './classes/DrawScene';
+import { DebugOrbitLine as Simple } from './classes/DebugOrbitLine';
+// import { DrawScene as Simple } from './classes/DrawScene';
 
 window.addEventListener('DOMContentLoaded', () => {
   new Simple();
